d2e5bf620f831c5b8d29866cd2513044bbdcb284
Ibrahima96 <ibrahimadiagne0811@gmail.com>
2026-05-30 11:47:51 +0000

    refactor: remove unused tasks API and update schema definitions

--- app/(shared-layout)/blog/page.tsx
+++ app/(shared-layout)/blog/page.tsx
@@ -2,12 +2,10 @@
 import { api } from "@/convex/_generated/api";
 import { useQuery } from "convex/react";
 const page = () => {
-  const tasks = useQuery(api.tasks.get);
+
   return (
     <main className="flex min-h-screen  items-center justify-between ">
-      {tasks?.map(({ _id, text }) => (
-        <div key={_id}>{text}</div>
-      ))}
+    
     </main>
   );
 };
