aae85b913ca82073b6b9465070d5bdebd645828e
Ibrahima96 <ibrahimadiagne0811@gmail.com>
2026-05-30 20:40:44 +0000

    fix: correct Navbar import case and update createBlogAction to include token in fetchMutation

--- app/actions.ts
+++ app/actions.ts
@@ -4,19 +4,27 @@ import z from "zod";
 import { postSchema } from "./schemas/blog";
 import { api } from "@/convex/_generated/api";
 import { redirect } from "next/navigation";
+import { getToken } from "@/lib/auth-server";
 
-export default async function createBlogAction( values: z.infer<typeof postSchema>) {
-
+export default async function createBlogAction(
+  values: z.infer<typeof postSchema>,
+) {
   const parsed = postSchema.safeParse(values);
 
   if (!parsed.success) {
     throw new Error("something went wrong");
   }
+  
+  const token = await getToken();
 
-  await fetchMutation(api.posts.createPost, {
-    title: parsed.data.title,
-    body: parsed.data.content,
-  },{});
+  await fetchMutation(
+    api.posts.createPost,
+    {
+      title: parsed.data.title,
+      body: parsed.data.content,
+    },
+    { token },
+  );
 
   return redirect("/");
 }
