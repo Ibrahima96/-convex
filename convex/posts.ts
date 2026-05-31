1d8f1373e53f10c21cd76127136da8f7d0aafac0
Ibrahima96 <ibrahimadiagne0811@gmail.com>
2026-05-31 12:51:52 +0000

    👀fix: authorId

--- convex/posts.ts
+++ convex/posts.ts
@@ -1,13 +1,19 @@
 import { mutation } from "./_generated/server";
-import { v } from "convex/values";
+import { ConvexError, v } from "convex/values";
+import { authComponent } from "./auth";
 
 // Create a new task with the given text
 export const createPost = mutation({
   args: { title: v.string(), body: v.string() },
   handler: async (ctx, args) => {
+    const user = await authComponent.safeGetAuthUser(ctx);
+    if (!user) {
+        throw new ConvexError("Not Authenticated !");
+    }
     const posts = await ctx.db.insert("posts", {
       title: args.title,
       body: args.body,
+      authorId: user._id,
     });
     return posts;
   },
