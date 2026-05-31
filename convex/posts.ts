2f1b7ebb8dfb8a2f894e7e2695154f07aa9dc8f0
Ibrahima96 <ibrahimadiagne0811@gmail.com>
2026-05-31 19:03:41 +0000

    usequery

--- convex/posts.ts
+++ convex/posts.ts
@@ -1,4 +1,4 @@
-import { mutation } from "./_generated/server";
+import { mutation, query } from "./_generated/server";
 import { ConvexError, v } from "convex/values";
 import { authComponent } from "./auth";
 
@@ -15,5 +15,13 @@ export const createPost = mutation({
       title: args.title,
       authorId: user._id,
     });
+  },
+});
+
+export const getPosts = query({
+  args: {},
+  handler: async (ctx) => {
+    const posts = await ctx.db.query("posts").order("desc").collect();
+    return posts
   },
 });
